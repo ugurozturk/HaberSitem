34b9fd38ecae0917f7e3778ee2308938df2496cf
Ugur Ozturk <ugur.ozturk@email.com>
2017-07-31 18:25:26 +0300

    Repository ler eklendi

---
+++ HaberSitem.Data/Repositories/UyelerRepository.cs
@@ -0,0 +1,13 @@
+﻿using HaberSitem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSitem.Data.Repositories
+{
+    public class UyelerRepository : BaseRepository<Uyeler>
+    {
+    }
+}
---
+++ HaberSitem.Data/Repositories/_BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using HaberSitem.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSitem.Data.Repositories
+{
+    public class BaseRepository<TEntity> where TEntity:class
+    {
+        HaberSitemContext context = new HaberSitemContext();
+        //Ekle,sil,guncelle,hepsinigetir,idyegoregetir
+        public void Ekle(TEntity nesne)
+        {
+            context.Set<TEntity>().Add(nesne);
+            context.SaveChanges();
+        }
+        public void Sil(TEntity nesne)
+        {
+            context.Set<TEntity>().Remove(nesne);
+            context.SaveChanges();
+        }
+        public void Guncelle(TEntity nesne)
+        {
+            context.Entry(nesne).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+        public List<TEntity> HepsiniGetir()
+        {
+            return context.Set<TEntity>().ToList();
+        }
+        public TEntity IdyeGoreGetir(int id)
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+    }
+}
